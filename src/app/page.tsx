6040c0ac6010a999ef31bb69ed388df276eef83c
Peter Muthama <muthamapeter79@gmail.com>
2025-02-28 12:41:58 +0300

    add logo image to home page and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black bg-[url('/sky.jpg')] bg-cover bg-center">
       <div className="flex flex-col justify-center items-center h-screen w-screen bg-black/[0.4]">
+      <Image
+      src={"/bonkelinks.svg"}
+      alt=""
+      width={150}
+      height={150}
+      />
         <p className="text-3xl text-white text-center">
           <span className="">BonkeLinks</span> is Under Construction
         </p>
